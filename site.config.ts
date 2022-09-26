421ddb13e689e54b6ae9c523943e2cb4613b25b1
batmaninrobin <malikmui@vcu.edu>
2022-09-25 21:00:26 -0400

    everything works, how to make resume its own subdomain not using notion?

--- site.config.ts
+++ site.config.ts
@@ -2,26 +2,26 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '92800663b742482093b9fa9c21b99ce2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Mani Malik Site',
+  domain: 'mani-malik.xyz',
+  author: 'Mohammad Malik',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Mani Malik\'s Site on Next.js and Notion',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  twitter: 'batmani_',
+  github: 'batmaniNrobin',
+  linkedin: 'mani-malik',
+  newsletter: 'https://hashnode.com/@mani-malik', // optional newsletter URL
+  youtube: 'channel/UCq397TiqIbXSlKItYlcuPpA/featured', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
