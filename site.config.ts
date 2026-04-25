20d268cf5013a11f998d7c0cd2c9d3255e440f31
batmaninrobin <imalikmu@gmail.com>
2026-04-25 11:35:30 -0400

    spaceId

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'b1b20d28-72f2-45dc-9ca0-ba36fd8880b7',
 
   // basic site info (required)
   name: 'Mani Malik Site',
